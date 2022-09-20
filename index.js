dd4dc47e2098365a5028c215c50fd94d457ded8b
Kimber Herron <kh582013@gmail.com>
2022-09-20 17:44:11 -0400

    commit tree 8fecaf11e4375921d3676f32133dc014a8ee18fd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickUp) {
+    if (pickUp >= 42) {
+        return (pickUp - 42)
+    }
+    else {
+        return (42 - pickUp)
+    }
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    console.log(distanceFromHqInBlocks(blockNumber));
+    return distanceFromHqInBlocks(blockNumber) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        return (start - destination) * 264
+    }
+    else {
+        return (destination - start) * 264}
+    }
+
+function calculatesFarePrice(startBlock, endBlock) {
+    let distance = distanceTravelledInFeet(startBlock, endBlock)
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000) {
+        return (distance - 400) * .02
+    } else if (distance <= 2500) {
+        return 25
+    } else {
+        return "cannot travel that far";
+    }
+}
+    
+
+
